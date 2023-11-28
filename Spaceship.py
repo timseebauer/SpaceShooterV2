668983926d5870307603a2042a8c7814aba34628
NBalleBS1 <144804295+NBalleBS1@users.noreply.github.com>
2023-11-28 14:40:20 +0100

    Update Spaceship.py

--- Spaceship.py
+++ Spaceship.py
@@ -1,7 +1,7 @@
 class Spaceship:
     def __init__(self, x, y):
-        self.x = 0
-        self.y = 0
+        self.x = Vec2.x
+        self.y = Vec2.y
         self.speed = 5
         self.hp = 5
         self.hitbox_radius = 3
