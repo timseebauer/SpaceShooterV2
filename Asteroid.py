0735776af07ef0c3283ff94bcbb34c2abbc77a07
NBalleBS1 <144804295+NBalleBS1@users.noreply.github.com>
2023-11-28 14:32:22 +0100

    Add files via upload

---
+++ Asteroid.py
@@ -0,0 +1,15 @@
+class Asteroid:
+    def __init__(self, x, y):
+        self.x = 0
+        self.y = 0
+        self.speed = 0
+        self.hitbox_radius = 3
+        self.hp = 3
+        self.damage = 1
+        self.sprite = None
+
+    def move(self):
+        pass
+
+    def split(self):
+        pass
