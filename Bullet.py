4ae22a02c2e8453502017c450f9dd27845c76a0b
lucasnegru <lu.negru@schueler.bs1-bt-de>
2023-11-28 14:51:58 +0100

    new Classes

---
+++ Bullet.py
@@ -0,0 +1,13 @@
+class Bullet:
+    def __int__(self, hitBoxRadius, dmg, sprite):
+        self.hitBoxRadius = hitBoxRadius
+        self.dmg = dmg
+        self.sprite = sprite
+
+
+def move(self):
+    pass
+
+
+def inflict_dmg(self):
+    pass
---
+++ Vec2.py
@@ -0,0 +1,5 @@
+class Vec2:
+    def __int__(self, x, y):
+        self.x = x
+        self.y = y
+        
